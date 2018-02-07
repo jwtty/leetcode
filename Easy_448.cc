d050dabc0fe26d743d65643537be3f5f97247b06
Wantong <1400012901@pku.edu.cn>
2018-02-07 15:10:44 +0800

    Add 448

---
+++ Easy_448.cc
@@ -0,0 +1,19 @@
+// Use the word itself (some redundant bits to represent the status)
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result;
+        for (int i = 0; i < n; ++i)
+        {
+            int index = abs(nums[i]) - 1;
+            nums[index] = nums[index] > 0 ? -nums[index] : nums[index];
+        }
+        for (int i = 0; i < n; ++i)
+        {
+            if(nums[i] > 0)
+                result.push_back(i + 1);
+        }
+        return result;
+    }
+};
